119f27bbbd7ddfbcf21d8209e05558a4b21ff6ff
Edona Sokoli <sokoliedona@gmail.com>
2020-05-01 14:54:03 +0000

    updated

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+# redirect, url_for
 from person import Person
 
 app = Flask(__name__)
@@ -13,6 +14,36 @@ def hello_world():
 @app.route('/profile')
 def profile():
     return 'This is the profile page.'
+
+"""
+@app.route('/profile')
+def profile():
+    return redirect(url_for('.index'))
+"""
+
+@app.route('/index')
+def index():
+    x = [10,20,30]
+    y = {'name' : 'Edona' , 'profession' : 'data analyst' }
+    z = ['hello' , 'everybody' ]
+
+    return render_template('index.html', NUMBERS = x, PERSON = y, WORDS = z)
+
+"""
+@app.route('/index')
+def index():
+    return render_template('index.html')
+"""
+@app.route('/forms', methods = ['GET', 'POST'])
+def forms():
+    if request.method == 'GET':
+
+        return render_template('forms.html')
+
+    elif request.method == 'POST':
+
+        name = request.form.get('last_name')
+        return f'hello {name}'
 
 
 @app.route('/table')
